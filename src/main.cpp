6326461ffe0bf765f87c11ac24fb554d71559d4a
pluveto <i@pluvet.com>
2021-10-06 13:12:26 +0800

    refa: load_env_levels

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <spdlog/spdlog.h>
+#include <spdlog/cfg/env.h>
+
 int
 main (int argc, char const *argv[])
 {
-  using namespace lb_lexer;
-  auto opt = app_option::from_env (argc, argv);
-  auto app = std::make_shared<lb_lexer::app> (opt);
+  spdlog::cfg::load_env_levels();
+  // Read arguments
+  auto opt = lb_lexer::app_option::from_env (argc, argv);
+  spdlog::info("Options loaded");
+  /* Create and run application */
+  auto app = std::make_shared<lb_lexer::app> (opt);    
   app->run ();
   return 0;
 }
